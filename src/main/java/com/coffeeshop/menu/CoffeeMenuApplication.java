d4d0f558339e56ddf38bdd7541c8a2f08f91de65
Ish Jaiswal <ishjaiswal7@gmail.com>
2025-02-20 18:51:20 +0530

    initial commit

---
+++ src/main/java/com/coffeeshop/menu/CoffeeMenuApplication.java
@@ -0,0 +1,13 @@
+package com.coffeeshop.menu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeMenuApplication.class, args);
+	}
+
+}
